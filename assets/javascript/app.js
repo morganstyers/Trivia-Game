fc5fa12cdd9ffc8d0b9a7c9b2185b2d0f4447188
Morgan Styers <morganstyers@gmail.com>
2019-08-16 23:51:06 -0400

    modify

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,17 @@
 $(document).ready(function(){
-  
-  
     $("#remaining-time").hide();
     $("#start").on('click', trivia.startGame);
-    $(document).on('click' , '.option', trivia.answerVal);
-
 })
 var trivia={
-timer: 30
+timer: 30,
 
-
-
-}
-
-
-
-function startGame(){
-    $("#game").show()
+startGame: function(){
+    $('#remaining-time').show();
     $('#timer').text(trivia.timer);
     $('#start').hide();
-    $('#remaining-time').show();
 }
+}
+
     //var questions
     //right/wrong/unanswered/timer/timerinfo
     //questions and answers
