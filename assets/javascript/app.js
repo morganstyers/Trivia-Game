87861b4969f2b8d339d7e974d73c80ce05b2d455
Morgan Styers <morganstyers@gmail.com>
2019-08-17 21:26:34 -0400

    modify

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,10 +12,15 @@ var trivia={
 correct: 0,
 incorrect:0,
 unanswered: 0,
-timer: 30,
 timerOn: false,
 
 startGame: function(){
+    timerOn=false;
+    var timer= 30;
+    if (!trivia.timerOn){
+    var intervalId = setInterval(countdown, 1000);
+    }
+    
     var questions=[ "question 1","question 1","question 1","question 1","question 1",];
     for(var i=0;i<questions.length;i++){
         var questionNumber= $("h4");
@@ -26,10 +31,18 @@ startGame: function(){
     $('#start').hide();
     $("#directions").hide();
     questionNumber.show("slow",function(){$("#q1").show('slow');});
-    $("#remaining-time").append("00:"+ trivia.timer).delay(1000).show("slow");
-},
-
+    $("#remaining-time").delay(1000).show("slow");
+    $("#timer").show(slow)("00: ");
+    
+    function countdown(){
+        timer--;
+        $("#timer").text(timer);
+        if(timer === 0){
+            clearInterval(intervalId)
+        }
+   
 
     
 }
     
+}}
