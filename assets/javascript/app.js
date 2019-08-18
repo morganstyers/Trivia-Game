44fde23477f8c72c056c1358644da38ff34bcc9b
Morgan Styers <morganstyers@gmail.com>
2019-08-18 18:54:22 -0400

    modify

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,29 @@
-$(document).ready(function(){
+
+$(document).ready(function () {
+    $("#results").hide();
+    $("#game").hide();
     $("#remaining-time").hide();
     $("#start").hide();
-    $("p").hide();
-    $("#choices").hide();
-    $("#start").slideUp().delay(3000).fadeIn();
-    $("#start").on('click', trivia.startGame);
-    
-    
-    
-})
-var trivia={
-correct: 0,
-incorrect:0,
-unanswered: 0,
-timerOn: false,
-
-startGame: function(){
-    timerOn=false;
-    var timer= 30;
-    if (!trivia.timerOn){
-    var intervalId = setInterval(countdown, 1000);
-    }
-    
-    var questions=[ "question 1","question 1","question 1","question 1","question 1",];
-    for(var i=0;i<questions.length;i++){
-        var questionNumber= $("h4");
-        questionNumber.text(questions[i]);
-        $("#question").prepend(questionNumber);
-    };
-    var choices=[]
-
-    $('#start').hide();
-    $("#directions").hide();
-    questionNumber.show("slow",function(){$("#choices").show('slow');});
-    $("#remaining-time").delay(1000).show("slow");
-    $("#timer").show(slow)("00: ");
-    
-    function countdown(){
-        timer--;
-        $("#timer").text(timer);
+    $("#start").slideUp().delay(3000).fadeIn(3000);
+    $(document).on('click', '#start', function () {
+        timerOn = false;
+        var timer = 30;
+        if (!timerOn) {
+            var intervalId = setInterval(countdown, 1000);
+        };
+        $('#start').hide();
+        $("#directions").hide();
+        $("#game").show();
+        $("#remaining-time").delay(1000).show("slow");
         if(timer === 0){
-            clearInterval(intervalId)
+            $("#results").show();
+        };
+        function countdown() {
+            timer--;
+            $("#timer").text(timer);
+            if (timer === 0) {
+                clearInterval(intervalId);
+            }
         }
-   
 
-    
-}
-    
-}}
+    })})
