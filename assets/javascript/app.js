36227ae79d10ff17f4a765e86bad7ac08f0e330a
Morgan Styers <morganstyers@gmail.com>
2019-08-19 17:48:05 -0400

    modify

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,6 @@ $(document).ready(function () {
     
     $(document).on('click', '#start', function () {
         timerOn = false;
-        var timer = 30;
         if (!timerOn) {
             var intervalId = setInterval(countdown, 1000);
         $("#timer").html("Time Remaining: " + time);}
@@ -26,7 +25,7 @@ $(document).ready(function () {
     function countdown() {
       $("#timer").html(time);
       time--;
-      if (time < 0) {
+      if (time === 0) {
         clearInterval(intervalId)
         $("#game").hide();
         $("#wins").html("Wins: " + winner).show()
@@ -38,74 +37,82 @@ $(document).ready(function () {
     $("ryan").click(function () {
      
       winner++;
-     
+     console.log()
     });
   
     $("#jim").click(function () {
       
       loser++;
       
-      
+      console.log()
     });
     $("#erin").click(function () {
       loser++;
+      console.log()
     });
   
     
     $("#richard").click(function () {
       loser++;
+      console.log()
     });
     
     $("#chris").click(function () {
       winner++;
-    
+      console.log()
     });
   
     $("#robin").click(function () {
       loser++;
+      console.log()
     });
   
   
   $("#hotdog").click(function () {
     loser++;
-    
+    console.log()
   });
   
   $("#hair").click(function () {
     winner++;
-  
+    console.log()
   });
   
   $("#dental").click(function () {
     loser++;
+    console.log()
   });
   
   //Question Four//
   $("#tuna").click(function () {
     winner++;
-    
+    console.log()
   });
   
   $("#halpert").click(function () {
     loser++;
+    console.log()
   });
   
   $("#jimbo").click(function () {
     loser++;
+    console.log()
   });
 
   //question 5
   $("#dumbledore").click(function () {
     winner++;
-    
+    console.log()
   });
   
   $("#stole").click(function () {
     loser++;
+    console.log()
   });
   
   $("#kidnapped").click(function () {
     loser++;
+    console.log()
   });
   
   
