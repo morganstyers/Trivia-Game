19c415eb27a6873a6f007aa39989dbb8565a3c48
Morgan Styers <morganstyers@gmail.com>
2019-09-05 22:10:51 -0400

    modify

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,121 +1,86 @@
 $(document).ready(function () {
-    $("#results").hide();
-    $("#game").hide();
-    $("#remaining-time").hide();
-    $("#start").hide();
-    $("#start").slideUp().delay(3000).fadeIn(3000);
-    var winner = 0;
-    var loser = 0;
-    var intervalId;
-    var time = 20;
-    $(document).on('click', '#start', function () {
-        timerOn = false;
-        if (!timerOn) {
-        var intervalId = setInterval(countdown, 1000);}
-        $("#timer").html("Time Remaining: " + time);
-       
-        
-        $('#start').hide();
-        $("#directions").hide();
-        $("#game").show();
-        $("#remaining-time").delay(1000).show("slow");
-    })
-    
-    function countdown() {
-      $("#timer").html(time);
-      time--;
-      if (time === 0) {
-        clearInterval(intervalId)
-        $("#remaining-time").empty();
-        $("#game").hide();
-        $("#wins").show(winner)
-        $("#losses").show(loser)
-      }
-    }
+  var intervalId, showQuestion;
+  var correctAnswer = false;
+  var time = 20;
+  var choiceGroup = ['<div><span id="button1" class="button" data-button-id="1"></span>', '<span id="button2" class="button" data-button-id="2"></span>', '<span id="button3" class="button" data-button-id="3"></span>', '<span id="button4" class="button" data-button-id="4"></span></div>'];
 
-    
-    $("ryan").click(function () {
-     
-      winner++;
-     console.log()
-    });
-  
-    $("#jim").click(function () {
-      
-      loser++;
-      
-      console.log()
-    });
-    $("#erin").click(function () {
-      loser++;
-      console.log()
-    });
-  
-    
-    $("#richard").click(function () {
-      loser++;
-      console.log()
-    });
-    
-    $("#chris").click(function () {
-      winner++;
-      console.log()
-    });
-  
-    $("#robin").click(function () {
-      loser++;
-      console.log()
-    });
-  
-  
-  $("#hotdog").click(function () {
-    loser++;
-    console.log()
-  });
-  
-  $("#hair").click(function () {
-    winner++;
-    console.log()
-  });
-  
-  $("#dental").click(function () {
-    loser++;
-    console.log()
-  });
-  
-  //Question Four//
-  $("#tuna").click(function () {
-    winner++;
-    console.log()
-  });
-  
-  $("#halpert").click(function () {
-    loser++;
-    console.log()
-  });
-  
-  $("#jimbo").click(function () {
-    loser++;
-    console.log()
-  });
+  var questions = [
+    {
+      question: "is this question one",
+      choices: ["yes", "no", "maybe", "idk"],
+      answer: '1',
+    },
+    {
+      question: "is this question two",
+      choices: ["yes", "no", "maybe", "idk"],
+      answer: '2',
+    },
+    {
+      question: "is this question three",
+      choices: ["yes", "no", "maybe", "idk"],
+      answer: '1',
+    },
+    {
+      question: "is this question three",
+      choices: ["yes", "no", "maybe", "idk"],
+      answer: '2',
+    },
+    {
+      question: "is this question five",
+      choices: ["idk", "no", "maybe", "sure"],
+      answer: '4',
+    },
+  ];
 
-  //question 5
-  $("#dumbledore").click(function () {
-    winner++;
-    console.log()
-  });
-  
-  $("#stole").click(function () {
-    loser++;
-    console.log()
-  });
-  
-  $("#kidnapped").click(function () {
-    loser++;
-    console.log()
-  });
-  
-  
-  
-  });
-  
+  var index=0;
+  var correct=0;
+  var incorrect=0;
+  var missed=0;
+
+  $("#game").hide();
+  $("#remaining-time").hide();
+  $("#start").hide();
+  $("#start").slideUp().delay(3000).fadeIn(3000);
+
+  $('#start').on('click', function () {
+    $('#start').hide();
+    $("#directions").hide();
+    $("#game").show();
+    display();
+  })
+
+  function display(){
+
+    $("#questions").append("<h4>"+questions[index].question+"</h4>");
+    for (let i = 0; i < questions[index].choices.length; i++) {
+      $(".buttons").append(choiceGroup[i]);
+      $(".buttons").append(" <button id='choice" + (i+1) + "'>" + questions[index].choices[i] + "</button>");
+
+    } 
+    $("#remaining-time").show();
+    run();
+  }
+ 
+
+$(".buttons").on('click', function(){
+  var buttonID = $(this).attr("data-button-id");
+  $("#questions").empty();
+
+})
+function run() {
+  intervalId = setInterval(decrement, 1000);
+}
+function decrement(){
+$("#timer").html(time);
+time--;
+if (time === 0){
+  stop();
+}
+}
+
+function stop(){
+  $("#timer").empty();
+  clearInterval(intervalId);
+}
+
+})
